b7754e08e1d32b9dace7164db5fc750a2bbd3679
barcenas <barcenas.developer@gmail.com>
2022-03-03 14:01:09 -0600

    UPDATE

--- app.ex
+++ app.ex
@@ -13,24 +13,9 @@ defmodule FizzBuzz do
   end
 
   def fizzBuzz(stop) do
-    numbers = 1..stop
-    numbers_list = Enum.to_list(numbers)
-    Enum.map(numbers_list, fn value -> number_or_text(value) end)
+    Enum.each(1..stop, fn value -> number_or_text(value) end)
   end
 
-  comment """
-  def print(evaluation) do
-    case evaluation do
-      {:Fizz} ->
-        IO.puts("Fizz")
-      {:Buzz} ->
-        IO.puts("Buzz")
-      _ ->
-        {val} = evaluation
-        IO.puts(val)
-    end
-  end
-  """
 end
 
 FizzBuzz.fizzBuzz(15)
